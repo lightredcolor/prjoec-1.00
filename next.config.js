58cd5b14beff831c77d6c153d7faad92261f287b
#FFF4F4 <192395288+lightredcolor@users.noreply.github.com>
2025-02-09 02:35:07 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/metadata.json',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig 
