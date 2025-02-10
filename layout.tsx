3c263b0b1a08f6020a3e825ddec1f7e67c31cdbc
#FFF4F4 <192395288+lightredcolor@users.noreply.github.com>
2025-02-10 17:48:53 +0500

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -2,7 +2,7 @@
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import "./globals.css";
 
-const manifestUrl = "https://github.com/lightredcolor/project-1.00/blob/main/metadata.json";
+const manifestUrl = "https://raw.githubusercontent.com/lightredcolor/project-1.00/refs/heads/main/metadata.json";
 
 export default function RootLayout({
   children,
